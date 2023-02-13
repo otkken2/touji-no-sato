f4f9677621232b956f40027a74e21f6701ea99f2
otkken2-scopenext <kenji.otaka@scopenext.jp>
2023-02-13 11:49:55 +0900

    ようやく画像付きの状態で記事を投稿できたところまで。

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -13,6 +13,8 @@ import { API_URL } from "const";
 import { Image, Post } from "interfaces";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { useAtom, useAtomValue } from "jotai";
+import { userAtom } from "@/atoms/atoms";
 
 interface PostInterface{
   description: string;
@@ -20,77 +22,91 @@ interface PostInterface{
       images: Image | undefined;
 }
 const Upload = () => {
-  const [selected, setSelected] = useState<null | LatLng>(null);
+  const [files, setFiles] = useState<any>();
+  const [ryokan, setRyokan] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const user = useAtomValue(userAtom);
   const [imageId, setImageId] = useState<number>(0);
-  const { control, handleSubmit } = useForm({
-    defaultValues: {
-      description: '',
-      ryokan: '',
-      images: undefined,
-    }
-  })
+  // const { control, handleSubmit } = useForm({
+  //   defaultValues: {
+  //     description: '',
+  //     ryokan: '',
+  //     images: undefined,
+  //   }
+  // })
+  const token = Cookies.get('token');
 
-  const onSubmit = async (data: PostInterface) => { 
-    console.log('投稿のdata')
-    console.log(data)
-    const token = Cookies.get('token');
-    const postData = new FormData();
-    if(data.images){
-      postData.append('files.images', data.images as unknown as Blob);      
-    }
+  // const onSubmit = async (data: PostInterface) => { 
+  //   console.log('投稿のdata')
+  //   console.log(data)
+  //   const postData = new FormData();
+  //   if(data.images){
+  //     postData.append('files.images', data.images as unknown as Blob);      
+  //   }
+
+  //   const textData = {
+  //     description: data.description,
+  //     ryokan: data.ryokan,
+  //   };
+
+  //   postData.append('data', JSON.stringify(textData));
+  //   await axios.post(`${API_URL}/api/posts`,postData,{
+  //     headers: {
+  //       Authorization: `Bearer ${token}`
+  //     }
+  //   }).then(res=> {
+  //     console.log(res)
+  //   }).catch(err => {
+  //     console.log('失敗')
+  //     console.log(err);
+  //   });
+  // }
+
+
+  const handleSubmit = async(e: any) => {
+    e.preventDefault();
+    console.log('handleSubmit')
+    console.log(files);
+    console.log(user);
+    const formData = new FormData();
+
+    formData.append('files.Image', files);
 
     const textData = {
-      description: data.description,
-      ryokan: data.ryokan,
-    };
-
-    postData.append('data', JSON.stringify(textData));
-    await fetch(`${API_URL}/api/posts`,{
+      ryokan: ryokan,
+      description: description,
+      // user: user?.username,
+    }
+    formData.append('data', JSON.stringify(textData));
+    await fetch(`${API_URL}/api/posts`, {
       method: 'post',
-      body: postData,
+      body: formData,
       headers:{
         Authorization: `Bearer ${token}`
       }
-    })
-    // return await axios.post(`${API_URL}/api/posts`,postData,{
-    //   headers: {
-    //     Authorization: `Bearer ${token}`
-    //   }
-    // }).then(res=> {
-    //   console.log(res)
-    // }).catch(err => {
-    //   console.log('失敗')
-    //   console.log(err);
-    // });
-  }
+    }).then(res => console.log('成功！'));
+  };
 
   return (
-    <form className="page-title" onSubmit={handleSubmit(onSubmit)}>
-      UPLOAD FROM THIS PAGE
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col">
-        <Controller
+        <TextField 
+          className="bg-white rounded-lg mb-8"
+          required
           name="ryokan"
-          control={control}
-          render={({ field })=>(
-            <TextField {...field} variant="filled" label='旅館・ホテル・温泉地を入力' className="bg-white rounded" placeholder="旅館・ホテル・温泉地の名前をみんなに教えてあげよう！"/>
-          )}
+          label='旅館・温泉名'
+          variant="filled"
+          onChange={e => setRyokan(e.target.value)}
         />
-        <Controller
+        <TextField 
+          required
+          className="bg-white rounded-lg mb-8"
           name="description"
-
-          control={control}
-          render={({field})=> (
-            <TextField {...field}  variant="filled" type="textarea" multiline minRows={5} className="bg-white rounded"/>
-            )}
-            />
-        <Typography>ファイルを選択してください</Typography>
-        <Controller
-          name="images"
-          control={control}
-          render={({field})=>(
-            <MuiFileInput {...field} className="bg-white rounded" variant="filled"/>
-            )}
+          label='本文'
+          variant="filled"
+          onChange={e => setDescription(e.target.value)}
         />
+        <MuiFileInput variant="filled" className="bg-white" onChange={e => setFiles(e)}/>
         <Button type="submit" variant="contained">投稿</Button>
       </div>
     </form>
