7f9b120a352bc9a5ff5772df19c7b2c212a78000
otkken2-scopenext <kenji.otaka@scopenext.jp>
2023-03-01 11:25:26 +0900

    検索結果の投稿を画面に表示できたところまで。

--- lib/useSearch.ts
+++ lib/useSearch.ts
@@ -1,17 +1,19 @@
+import { PostData } from "@/Interface/interfaces";
 import axios from "axios";
 import { API_URL } from "const";
+import { useState } from "react";
 
 const useSearch = () => {
+  const [ searchResults, setSearchResults ] = useState<PostData[]>([]);
   const search = async(searchWord: string) => {
     const searchResultsOfPosts = await axios.get(
-      `${API_URL}/api/posts?populate=*
-      &filters[$or][0][description][$contains]=${searchWord}&filters[$or][1][ryokan][$contains]=${searchWord}
+      `${API_URL}/api/posts?populate=*&filters[$or][0][description][$contains]=${searchWord}&filters[$or][1][ryokan][$contains]=${searchWord}
       `
-    ).then(res => res.data);
+    ).then(res => res.data.data);
 
-    console.log(searchResultsOfPosts);
+    setSearchResults(searchResultsOfPosts);
   };
-  return {search}
+  return {search,searchResults}
 };
 
 export default useSearch;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,21 +1,31 @@
+import { Post } from "@/components/Post/Post";
 import { Button, TextField } from "@mui/material";
 import useSearch from "lib/useSearch";
 import { useState } from "react";
 const Search = ()=>{
   const [searchWord, setSearchWord] = useState('')
-  const {search} = useSearch();
+  const {search, searchResults} = useSearch();
   const handleSubmit = (e:any) => {
     // alert(searchWord);
     e.preventDefault();
     search(searchWord);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex flex-col">
-        <TextField variant="filled" label='検索ワードを入力してください' onChange={(e)=> setSearchWord(e.target.value)}/>
-        <Button variant="contained" type="submit">検索</Button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col">
+          <TextField variant="filled" label='検索ワードを入力してください' onChange={(e)=> setSearchWord(e.target.value)}/>
+          <Button variant="contained" type="submit">検索</Button>
+        </div>
+      </form>
+      {searchResults.length > 0 &&
+        searchResults.map((searchResult,index)=>{
+          return (
+            <Post key={index} post={searchResult} index={index}/>
+          );
+        })
+      }
+    </>
   );
 };
 
