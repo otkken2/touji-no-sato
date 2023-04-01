19226317991d1853bc010c9941d8def041bb7536
otkken2 <lovelovezamakko2@gmail.com>
2023-04-01 18:43:19 +0900

    mediaUrlsOfPostテーブルにレコードを作成できるようにした。（byChatGPT!）

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -39,63 +39,137 @@ const Upload = () => {
       body: formData,
     });
     const uploadedFile = await response.json();
+    if(uploadedFile)console.log('ファイルアップロードされたよ');
+    console.log("uploadedFile in uploadMediaFile()");
+    console.log(uploadedFile);
+    setUploadedFiles(prev => [...prev,uploadedFile[0]]);
     return uploadedFile;
   };
 
-  const handleSubmit = async(e: any) => {
+  // const handleSubmit = async(e: any) => {
+  //   e.preventDefault();
+  //   const formData = new FormData();
+
+  //   if(files){
+  //     console.log('画像ファイルあるよ');
+  //     files.map(async(file) => {
+  //       const uploadedFile: ImageAttributes[] = await uploadMediaFile(file);
+  //       // setUploadedFiles(prev => [...prev,uploadedFile[0]]);
+        
+  //     });
+  //   }
+    
+  //   const textData = {
+  //     ryokan: selectedPlace,
+  //     description: description,
+  //     user: user?.id,
+  //     lat: lat,
+  //     lng: lng,
+  //   }
+  //   formData.append('data', JSON.stringify(textData));
+  //   await fetch(`${API_URL}/api/posts`, {
+  //     method: 'post',
+  //     body: formData,
+  //     headers:{
+  //       Authorization: `Bearer ${token}`
+  //     }
+  //   }).then(async res => {
+  //     const post: Post = await res.json();
+  //     if(!post)return;
+  //     const postId = post.data?.id;
+  //     if(!uploadedFiles)return;
+  //     console.log('useStateのuploadedFilesあるよ');
+  //     console.log("uploadedFiles↓");
+  //     console.log(uploadedFiles);
+  //     uploadedFiles.map(async eachFile => {
+  //       const data = {
+  //         postId: postId,
+  //         mediaAssetId: eachFile.id,
+  //       };
+
+  //       await axios.post(`${API_URL}/api/media-urls-of-posts`,
+  //         {
+  //           data: data,
+  //         },
+  //         {
+  //           headers:
+  //             {
+  //               Authorization: `Bearer ${token}`
+  //             }
+  //         }
+  //         ).then(res =>console.log('mediaUrlsOfPostsの投稿に成功しました'))
+  //         .catch(err => console.log(err));
+  //     })
+  //     // alert('Postの投稿に成功しました');
+  //     setUploadedFiles([]);
+  //     // router.push('/');
+  //   });
+  // };
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     const formData = new FormData();
-
-    if(files){
-      files.map(async(file) => {
-        const uploadedFile: ImageAttributes[] = await uploadMediaFile(file);
-        setUploadedFiles(prev => [...prev,uploadedFile[0]]);
-      });
+  
+    let flattenedUploadedFiles: ImageAttributes[] = [];
+  
+    if (files) {
+      console.log("画像ファイルあるよ");
+      const uploadedFilesPromises = files.map((file) => uploadMediaFile(file));
+      const allUploadedFiles: ImageAttributes[][] = await Promise.all(uploadedFilesPromises);
+      flattenedUploadedFiles = allUploadedFiles.flat();
     }
-    
+  
     const textData = {
       ryokan: selectedPlace,
       description: description,
       user: user?.id,
       lat: lat,
       lng: lng,
-    }
-    formData.append('data', JSON.stringify(textData));
+    };
+    formData.append("data", JSON.stringify(textData));
     await fetch(`${API_URL}/api/posts`, {
-      method: 'post',
+      method: "post",
       body: formData,
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
-    }).then(async res => {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(async (res) => {
       const post: Post = await res.json();
-      if(!post)return;
+      if (!post) return;
       const postId = post.data?.id;
-      if(!uploadedFiles)return;
-      uploadedFiles.map(async eachFile => {
+      if (!flattenedUploadedFiles) return;
+      console.log("flattenedUploadedFilesあるよ");
+      console.log("flattenedUploadedFiles↓");
+      console.log(flattenedUploadedFiles);
+      for (const eachFile of flattenedUploadedFiles) {
         const data = {
           postId: postId,
           mediaAssetId: eachFile.id,
         };
-
-        await axios.post(`${API_URL}/api/media-urls-of-posts`,
-          {
-            data: data,
-          },
-          {
-            headers:
-              {
-                Authorization: `Bearer ${token}`
-              }
-          }
-          ).then(res =>console.log('mediaUrlsOfPostsの投稿に成功しました'))
-          .catch(err => console.log(err));
-      })
-      alert('Postの投稿に成功しました');
+  
+        await axios
+          .post(
+            `${API_URL}/api/media-urls-of-posts`,
+            {
+              data: data,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then((res) => console.log("mediaUrlsOfPostsの投稿に成功しました"))
+          .catch((err) => console.log(err));
+      }
+      // alert("Postの投稿に成功しました");
       setUploadedFiles([]);
-      router.push('/');
+      // router.push("/");
     });
   };
+  
+  
+  
   
   return (
     <UploadForm handleSubmit={handleSubmit} title='新規投稿'/>
