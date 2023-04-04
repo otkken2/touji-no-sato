0e15dc05d7408a78aa1dedf5cdfee8d9224a7dd6
otkken2 <lovelovezamakko2@gmail.com>
2023-04-04 12:32:21 +0900

    お気に入りページのレイアウトを修正

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -10,7 +10,8 @@ const Favorite = () => {
 
   return (
     myFavorites &&
-      <div>
+      <div className='max-w-[600px] mx-auto'>
+        <h1 className=' text-white text-center my-5 text-2xl'>お気に入り</h1>
         {myFavorites.map((myFavorite)=>{
           return(
             <Post key={myFavorite.attributes?.post?.data?.id} post={myFavorite?.attributes?.post?.data} index={myFavorite.attributes?.post?.data?.id}/>
