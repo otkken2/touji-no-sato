a32ea2666f22b55ca943a77669ade1301083c1ee
otkken2 <lovelovezamakko2@gmail.com>
2023-04-08 17:29:04 +0900

    API_URL修正

--- const.ts
+++ const.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337" || "https://toujinosato.herokuapp.com";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://toujinosato.herokuapp.com" || "http://localhost:1337";
