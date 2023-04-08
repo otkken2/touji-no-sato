a86494bc99aa319aec054199ed90731dc2ce1dc4
otkken2 <lovelovezamakko2@gmail.com>
2023-04-08 21:01:50 +0900

    デバッグ

--- const.ts
+++ const.ts
@@ -1,11 +1,10 @@
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://toujinosato.herokuapp.com" || "http://localhost:1337";
 export const API_URL = (() => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NEXT_PUBLIC_NODE_ENV === "staging") {
+      return "https://toujinosato.herokuapp.com";
+    } else if (process.env.NODE_ENV === "production") {
       return "https://toujinosato.com";
     } else if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
       return "http://localhost:1337";
-    } else if (process.env.NEXT_PUBLIC_NODE_ENV === "staging") {
-      return "https://toujinosato.herokuapp.com";
     } else {
       throw new Error("Unknown NODE_ENV");
     }
--- src/pages/profile/[id]/index.tsx
+++ src/pages/profile/[id]/index.tsx
@@ -142,6 +142,8 @@ export const Profile = () => {
       </div>
     }
   };
+  console.log("process.env.NEXT_PUBLIC_NODE_ENV↓")
+  console.log(process.env.NEXT_PUBLIC_NODE_ENV)
 
   return (
     <main className='text-white'>
