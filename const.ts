bfa9e9f3134fe74182ccf31056eef2bbe1813ab6
otkken2 <lovelovezamakko2@gmail.com>
2023-04-08 21:18:20 +0900

    デバッグ修正

--- const.ts
+++ const.ts
@@ -1,7 +1,7 @@
 export const API_URL = (() => {
     if (process.env.NEXT_PUBLIC_NODE_ENV === "staging") {
       return "https://toujinosato.herokuapp.com";
-    } else if (process.env.NODE_ENV === "production") {
+    } else if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
       return "https://toujinosato.com";
     } else if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
       return "http://localhost:1337";
