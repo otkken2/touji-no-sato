e9b2c2bdcd636dc78e3bd55f031bd022653648dd
otkken2 <lovelovezamakko2@gmail.com>
2023-08-11 21:57:53 +0900

    ヘッダーロゴとOGP用イメージを更新

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,8 @@ const Header = () => {
     <div className="flex justify-between items-center">
       <h1>
         <Link href="/">
-          <Image src={"/logo.svg"} alt="湯治の郷" width={148} height={55} className='my-5 ml-3'/>
+          {/* <Image src={"/logo.svg"} alt="湯治の郷" width={148} height={55} className='my-5 ml-3'/> */}
+          <Image src={"/logo_toujinosato.png"} alt="湯治の郷" width={148} height={55} className='my-5 ml-3'/>
         </Link>
       </h1>
       <Link href={`/profile/${user?.id}`}>
